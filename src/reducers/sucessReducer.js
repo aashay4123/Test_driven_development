670cfe31d324163aca8f539c33499fe293942536
Aashay <aashayphirke5798@gmail.com>
2020-06-11 02:57:04 +0530

    redux reducers test

--- src/reducers/sucessReducer.js
+++ src/reducers/sucessReducer.js
@@ -1,3 +1,10 @@
-export default (state, Action) => {
-  return null;
+import { actionTypes } from "../actions";
+
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
